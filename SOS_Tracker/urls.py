ceb745e8ddafc5f7b3fde37545455e74b809bb10
DominikPyrek <pyrekdominik2025@gmail.com>
2025-05-28 23:42:52 +0200

    Fixed .

--- SOS_Tracker/urls.py
+++ SOS_Tracker/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('/devices/<int:id>/assign/', views.assign_device_to_user),
     path('/devices/<int:id>/location/', views.new_location_ping),
     path('/users/<int:id>/location/', views.get_user_location),
-    path('/map/', views,latest_location_of_all_devices),
+    path('/map/', views.latest_location_of_all_devices),
 ]
